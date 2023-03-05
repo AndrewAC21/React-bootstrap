d29617246ebf32df56d4fd34288c5a2cee645206
AndrewAC21 <88219834+AndrewAC21@users.noreply.github.com>
2023-03-05 06:20:30 +0000

    create card styles

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,16 +3,25 @@ import Card from "react-bootstrap/Card";
 import CartSvg from "./CartSvg";
 
 export default function Product({ product }) {
+  let title = product.title.split(" ")
+  let newTitle = title.slice(0, 7).join(" ")
+  
   return (
-    <Card>
-      <Card.Img src={product.image} alt="" className="product-img" />
+    <Card className="product">
+      <div className="product__img-container">
+      <Card.Img src={product.image} alt="" className="product__img" />
+      </div>
+      <div className="product__info">
       <Card.Body>
-        <Card.Title>{product.title}</Card.Title>
+        <Card.Title>{newTitle}</Card.Title>
+        <div className="product__sub-info">
         <Card.Text>${product.price}</Card.Text>
-        <div className="product__svg">
+        <div className="sub-info__svg">
           <CartSvg />
         </div>
+        </div>
       </Card.Body>
+      </div>
     </Card>
   );
 }
